ea8155b2805d29b781383bd1e1582fdb3a2d4838
Joao Ferreira <26050@ipportalegre.pt>
2025-10-19 23:33:48 +0100

    feat: adiciona função percentagem com validação de valores negativos

--- calc.py
+++ calc.py
@@ -11,3 +11,8 @@ def divisao(a, b):
 	if b == 0:
 		raise ValueError("Erro: Mão é possível dividir por 0")
 	return a / b
+
+def percentagem(a, b):
+    if a < 0 or b < 0:
+        raise ValueError("Erro: Percentagem não aceita valores negativos!")
+    return (a * b) / 100
